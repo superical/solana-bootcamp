47aa70a057d05a99a4ecd8b8d524c6c8d079ce50
Seth <hullogh@gmail.com>
2024-04-03 04:16:31 +0800

    feat: homework6 – option

--- homeworks_rust/homeworks/homework6/option/option3.rs
+++ homeworks_rust/homeworks/homework6/option/option3.rs
@@ -1,8 +1,6 @@
 // option3.rs
 // Make me compile! Execute `rustlings hint option3` for hints
 
-// I AM NOT DONE
-
 struct Point {
     x: i32,
     y: i32,
@@ -11,7 +9,11 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    // Notes to self:
+    // Apparently, when accessing the `Some` value of an `Option`, the value will get moved out.
+    // The variable `p` took the ownership of the `Point` value when accessing the `Some` of `y`, so `y` can't be used anymore
+    // unless we pass `y` by ref then `p` itself is also a ref.
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => println!("no match"),
     }
